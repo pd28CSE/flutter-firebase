7beb84dec845fd7570ee076602ecd0c349da2b80
Partho Debnath <parthodebnath28@gmail.com>
2023-08-18 15:42:28 +0600

    get data from firebase Firestore-Database/Cloud-Firestore

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:developer';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 import 'package:firebase_core/firebase_core.dart';
@@ -30,13 +33,96 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  late FirebaseFirestore firebaseFirestore;
+
+  @override
+  void initState() {
+    super.initState();
+    firebaseFirestore = FirebaseFirestore.instance;
+    // WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+    //   getValue();
+    // });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: const Center(child: Text('Flutter Firebase Manual Connection')),
+      body: FutureBuilder(
+        future: firebaseFirestore
+            .collection('basketball')
+            .doc('1_ban_vs_nep')
+            .get(),
+        builder: (cntxt, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator.adaptive());
+          } else if (snapshot.hasError == true) {
+            return const Center(child: Text('Error..'));
+          } else if (snapshot.hasData == true) {
+            var matchInfo = snapshot.data!.data();
+            return Column(
+              children: <Widget>[
+                const SizedBox(height: 50),
+                Text(matchInfo!['match_name'],
+                    style: Theme.of(context).textTheme.displaySmall),
+                const SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Column(
+                      children: <Widget>[
+                        Text(matchInfo['score_team_a'].toString()),
+                        Text(matchInfo['team_a']),
+                      ],
+                    ),
+                    const Text('vs'),
+                    Column(
+                      children: <Widget>[
+                        Text(matchInfo['score_team_b'].toString()),
+                        Text(matchInfo['team_b']),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            );
+          } else {
+            return const Center(child: Text('sgsg'));
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          await firebaseFirestore
+              .collection('basketball')
+              .doc('1_ban_vs_nep')
+              .update({
+            'team_b': 'Nepal',
+            'score_team_b': 30,
+          });
+          if (mounted) {
+            setState(() {});
+          }
+        },
+        child: const Text('Updata'),
+      ),
     );
   }
+
+  // Future<void> getValue() async {
+  //   log('-*******');
+  //   CollectionReference collectionReference =
+  //       firebaseFirestore.collection('basketball');
+  //   DocumentReference documentReferences =
+  //       collectionReference.doc('1_ban_vs_nep');
+  //   DocumentSnapshot info = await documentReferences.get();
+  //   log(info.data().toString());
+  //   log(info.get('match_name'));
+  //   log(info.get('team_a'));
+  //   log(info.get('score_team_a').toString());
+  //   log(info.get('team_b'));
+  //   log(info.get('score_team_b').toString());
+  // }
 }
