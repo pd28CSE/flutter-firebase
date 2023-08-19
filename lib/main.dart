e5adad8dba39347fe0fd749957e01af83ba0f1da
Partho Debnath <parthodebnath28@gmail.com>
2023-08-19 14:09:08 +0600

    get info  from firebase 'Cloud Firestore' with streamBuilder

--- lib/main.dart
+++ lib/main.dart
@@ -39,9 +39,10 @@ class _MyHomePageState extends State<MyHomePage> {
   void initState() {
     super.initState();
     firebaseFirestore = FirebaseFirestore.instance;
-    // WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
-    //   getValue();
-    // });
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      addMatchInfo();
+      getValue();
+    });
   }
 
   @override
@@ -50,68 +51,123 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: FutureBuilder(
-        future: firebaseFirestore
-            .collection('basketball')
-            .doc('1_ban_vs_nep')
-            .get(),
-        builder: (cntxt, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator.adaptive());
-          } else if (snapshot.hasError == true) {
-            return Center(child: Text(snapshot.error.toString()));
-          } else if (snapshot.hasData == true && !snapshot.data!.exists) {
-            return const Center(child: Text("Document does not exist"));
-          } else if (snapshot.connectionState == ConnectionState.done) {
-            Map<String, dynamic>? matchInfo = snapshot.data?.data();
-            return Column(
-              children: <Widget>[
-                const SizedBox(height: 50),
-                Text(matchInfo?['match_name'] ?? '',
-                    style: Theme.of(context).textTheme.displaySmall),
-                const SizedBox(height: 20),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                    Column(
-                      children: <Widget>[
-                        Text(matchInfo?['score_team_a'].toString() ?? ''),
-                        Text(matchInfo?['team_a'] ?? ''),
-                      ],
-                    ),
-                    const Text('vs'),
-                    Column(
-                      children: <Widget>[
-                        Text(matchInfo?['score_team_b'].toString() ?? ''),
-                        Text(matchInfo?['team_b'] ?? ''),
-                      ],
-                    ),
-                  ],
-                ),
-              ],
-            );
-          } else {
-            return const Center(child: Text('Other Info...'));
-          }
-        },
-      ),
+      body: withStreamBuilder(context),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
-          // await firebaseFirestore
-          //     .collection('basketball')
-          //     .doc('1_ban_vs_nep')
-          //     .update({
-          //   'team_b': 'Nepal',
-          //   'score_team_b': 30,
-          // });
+          await firebaseFirestore
+              .collection('basketball')
+              .doc('1_ban_vs_nep')
+              .update({
+            'team_b': 'Nepal',
+            'score_team_b': 30,
+          });
           // if (mounted) {
           //   setState(() {});
           // }
           // await addNewMatchWithCustomDocumentName();
-          await addNewMatchWithAutoDocumentName();
+          // await addNewMatchWithAutoDocumentName();
         },
         child: const Icon(Icons.update_sharp),
       ),
+    );
+  }
+
+  StreamBuilder<DocumentSnapshot<Map<String, dynamic>>> withStreamBuilder(
+      BuildContext context) {
+    return StreamBuilder<DocumentSnapshot<Map<String, dynamic>>>(
+      stream: firebaseFirestore
+          .collection('basketball')
+          .doc('1_ban_vs_nep')
+          .snapshots(),
+      builder: (cntxt,
+          AsyncSnapshot<DocumentSnapshot<Map<String, dynamic>>> snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator.adaptive());
+        } else if (snapshot.hasError == true) {
+          return Center(child: Text(snapshot.error.toString()));
+        } else if (snapshot.hasData == true && snapshot.data!.exists == false) {
+          return const Center(child: Text("Document does not exist"));
+        } else if (snapshot.data != null) {
+          Map<String, dynamic>? matchInfo = snapshot.data!.data();
+          log(matchInfo.toString());
+          return Column(
+            children: <Widget>[
+              const SizedBox(height: 50),
+              Text(matchInfo?['match_name'] ?? '',
+                  style: Theme.of(context).textTheme.displaySmall),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Column(
+                    children: <Widget>[
+                      Text(matchInfo?['score_team_a'].toString() ?? ''),
+                      Text(matchInfo?['team_a'] ?? ''),
+                    ],
+                  ),
+                  const Text('vs'),
+                  Column(
+                    children: <Widget>[
+                      Text(matchInfo?['score_team_b'].toString() ?? ''),
+                      Text(matchInfo?['team_b'] ?? ''),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          );
+        } else {
+          return const Center(child: Text('Other Info...'));
+        }
+      },
+    );
+  }
+
+  FutureBuilder<DocumentSnapshot<Map<String, dynamic>>> withFutureBuilder(
+      BuildContext context) {
+    return FutureBuilder<DocumentSnapshot<Map<String, dynamic>>>(
+      future:
+          firebaseFirestore.collection('basketball').doc('1_ban_vs_nep').get(),
+      builder: (cntxt,
+          AsyncSnapshot<DocumentSnapshot<Map<String, dynamic>>> snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator.adaptive());
+        } else if (snapshot.hasError == true) {
+          return Center(child: Text(snapshot.error.toString()));
+        } else if (snapshot.hasData == true && !snapshot.data!.exists) {
+          return const Center(child: Text("Document does not exist"));
+        } else if (snapshot.connectionState == ConnectionState.done) {
+          Map<String, dynamic>? matchInfo = snapshot.data?.data();
+          return Column(
+            children: <Widget>[
+              const SizedBox(height: 50),
+              Text(matchInfo?['match_name'] ?? '',
+                  style: Theme.of(context).textTheme.displaySmall),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Column(
+                    children: <Widget>[
+                      Text(matchInfo?['score_team_a'].toString() ?? ''),
+                      Text(matchInfo?['team_a'] ?? ''),
+                    ],
+                  ),
+                  const Text('vs'),
+                  Column(
+                    children: <Widget>[
+                      Text(matchInfo?['score_team_b'].toString() ?? ''),
+                      Text(matchInfo?['team_b'] ?? ''),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          );
+        } else {
+          return const Center(child: Text('Other Info...'));
+        }
+      },
     );
   }
 
@@ -179,39 +235,52 @@ class _MyHomePageState extends State<MyHomePage> {
       //     .delete();
     }
 
-    final DocumentReference documentReference =
-        firebaseFirestore.collection('basketball').doc('CCn9rW90cWZw0iMD2Ef6');
-    DocumentSnapshot<Object?> documentSnapshot = await documentReference.get();
-    if (documentSnapshot.data() != null) {
-      Map<String, dynamic> data =
-          documentSnapshot.data() as Map<String, dynamic>;
-      log(data.toString());
-    }
+    // final DocumentReference documentReference =
+    //     firebaseFirestore.collection('basketball').doc('CCn9rW90cWZw0iMD2Ef6');
+    // DocumentSnapshot<Object?> documentSnapshot = await documentReference.get();
+    // if (documentSnapshot.data() != null) {
+    //   Map<String, dynamic> data =
+    //       documentSnapshot.data() as Map<String, dynamic>;
+    //   log(data.toString());
+    // }
 
     //? update that document data if the field already exists otherwise create a new field.
     //? if that document does not exist then throw an error.
-    documentReference.update({
-      'full_name': 'Debnath  Partho',
-    }).catchError((val) {
-      log(val.toString());
-    });
+    // documentReference.update({
+    //   'full_name': 'Debnath  Partho',
+    // }).catchError((val) {
+    //   log(val.toString());
+    // });
 
     //? delete that document if exists otherwise nothing to do.
     // await documentReference.delete();
   }
 
-  // Future<void> getValue() async {
-  //   log('-*******');
-  //   CollectionReference collectionReference =
-  //       firebaseFirestore.collection('basketball');
-  //   DocumentReference documentReferences =
-  //       collectionReference.doc('1_ban_vs_nep');
-  //   DocumentSnapshot info = await documentReferences.get();
-  //   log(info.data().toString());
-  //   log(info.get('match_name'));
-  //   log(info.get('team_a'));
-  //   log(info.get('score_team_a').toString());
-  //   log(info.get('team_b'));
-  //   log(info.get('score_team_b').toString());
-  // }
+  Future<void> addMatchInfo() async {
+    CollectionReference collectionReference =
+        firebaseFirestore.collection('basketball');
+    await collectionReference.doc('1_ban_vs_nep').set(
+      {
+        'match_name': 'Bang vs Nepal',
+        'team_a': 'Bangladesh',
+        'score_team_a': 1,
+        'team_b': 'Nepal',
+        'score_team_b': 10,
+      },
+    );
+  }
+
+  Future<void> getValue() async {
+    CollectionReference collectionReference =
+        firebaseFirestore.collection('basketball');
+    DocumentReference documentReferences =
+        collectionReference.doc('1_ban_vs_nep');
+    DocumentSnapshot info = await documentReferences.get();
+    log(info.data().toString());
+    log(info.get('match_name'));
+    log(info.get('team_a'));
+    log(info.get('score_team_a').toString());
+    log(info.get('team_b'));
+    log(info.get('score_team_b').toString());
+  }
 }
