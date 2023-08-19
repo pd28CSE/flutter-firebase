903055878a471b06404b142882e954352b00fabe
Partho Debnath <parthodebnath28@gmail.com>
2023-08-19 12:36:43 +0600

    add custom/auto_generated document, add new data, get all document and data, update data, delete document from firebase 'Cloud Firestore'

--- lib/main.dart
+++ lib/main.dart
@@ -59,13 +59,15 @@ class _MyHomePageState extends State<MyHomePage> {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return const Center(child: CircularProgressIndicator.adaptive());
           } else if (snapshot.hasError == true) {
-            return const Center(child: Text('Error..'));
-          } else if (snapshot.hasData == true) {
-            var matchInfo = snapshot.data!.data();
+            return Center(child: Text(snapshot.error.toString()));
+          } else if (snapshot.hasData == true && !snapshot.data!.exists) {
+            return const Center(child: Text("Document does not exist"));
+          } else if (snapshot.connectionState == ConnectionState.done) {
+            Map<String, dynamic>? matchInfo = snapshot.data?.data();
             return Column(
               children: <Widget>[
                 const SizedBox(height: 50),
-                Text(matchInfo!['match_name'],
+                Text(matchInfo?['match_name'] ?? '',
                     style: Theme.of(context).textTheme.displaySmall),
                 const SizedBox(height: 20),
                 Row(
@@ -73,15 +75,15 @@ class _MyHomePageState extends State<MyHomePage> {
                   children: <Widget>[
                     Column(
                       children: <Widget>[
-                        Text(matchInfo['score_team_a'].toString()),
-                        Text(matchInfo['team_a']),
+                        Text(matchInfo?['score_team_a'].toString() ?? ''),
+                        Text(matchInfo?['team_a'] ?? ''),
                       ],
                     ),
                     const Text('vs'),
                     Column(
                       children: <Widget>[
-                        Text(matchInfo['score_team_b'].toString()),
-                        Text(matchInfo['team_b']),
+                        Text(matchInfo?['score_team_b'].toString() ?? ''),
+                        Text(matchInfo?['team_b'] ?? ''),
                       ],
                     ),
                   ],
@@ -89,26 +91,113 @@ class _MyHomePageState extends State<MyHomePage> {
               ],
             );
           } else {
-            return const Center(child: Text('sgsg'));
+            return const Center(child: Text('Other Info...'));
           }
         },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
-          await firebaseFirestore
-              .collection('basketball')
-              .doc('1_ban_vs_nep')
-              .update({
-            'team_b': 'Nepal',
-            'score_team_b': 30,
-          });
-          if (mounted) {
-            setState(() {});
-          }
+          // await firebaseFirestore
+          //     .collection('basketball')
+          //     .doc('1_ban_vs_nep')
+          //     .update({
+          //   'team_b': 'Nepal',
+          //   'score_team_b': 30,
+          // });
+          // if (mounted) {
+          //   setState(() {});
+          // }
+          // await addNewMatchWithCustomDocumentName();
+          await addNewMatchWithAutoDocumentName();
         },
-        child: const Text('Updata'),
+        child: const Icon(Icons.update_sharp),
       ),
     );
+  }
+
+  Future<void> addNewMatchWithCustomDocumentName({String? documentName}) async {
+    //? Add new document in the collection "basketball"
+    final DocumentReference documentReference = firebaseFirestore
+        .collection('basketball')
+        .doc(documentName ?? 'custom_document_name');
+
+    //?   add new data in that document.
+    await documentReference.set(
+      {
+        'full_name': 'Partho Debnath',
+        'city': 'Mirpur',
+        'document_type': 'Custom DocumentName.'
+      },
+    );
+
+    //? read that document data
+    final DocumentSnapshot<Object?> documentSnapshot =
+        await documentReference.get();
+    if (documentSnapshot.data() != null) {
+      Map<String, dynamic> data =
+          documentSnapshot.data() as Map<String, dynamic>;
+      log(data.toString());
+    }
+
+    //? update that document data if the field already exists otherwise create a new field.
+    //? if that document does not exist then throw an error.
+    // documentReference.update({
+    //   'full_name': 'Debnath Partho',
+    // }).catchError((val) {
+    //   log(val.toString());
+    // });
+
+    //? delete that document if exists otherwise nothing to do.
+    // await documentReference.delete();
+  }
+
+  Future<void> addNewMatchWithAutoDocumentName() async {
+    //? in the collection "basketball" and auto generated  document
+    await firebaseFirestore.collection('basketball').add(
+      {
+        'full_name': 'Partho Debnath',
+        'city': 'Mirpur',
+        'document_type': 'Auto Generated Document Name.'
+      },
+    );
+
+    //? get all the documents from the "basketball" collection
+    QuerySnapshot<Map<String, dynamic>> data =
+        await firebaseFirestore.collection('basketball').get();
+
+    //? get total number of documents from the "basketball" collection
+    int lengthOfDoc = data.docs.length;
+    for (int i = 0; i < lengthOfDoc; i++) {
+      log(data.docs[i].get('full_name'));
+      Map<String, dynamic> info = data.docs[i].data();
+      String documentName = data.docs[i].id;
+      log('$i: $documentName: $info');
+      //   //? delete add documents from the "basketball" collection
+      // await firebaseFirestore
+      //     .collection('basketball')
+      //     .doc(documentName)
+      //     .delete();
+    }
+
+    final DocumentReference documentReference =
+        firebaseFirestore.collection('basketball').doc('CCn9rW90cWZw0iMD2Ef6');
+    DocumentSnapshot<Object?> documentSnapshot = await documentReference.get();
+    if (documentSnapshot.data() != null) {
+      Map<String, dynamic> data =
+          documentSnapshot.data() as Map<String, dynamic>;
+      log(data.toString());
+    }
+
+    //? update that document data if the field already exists otherwise create a new field.
+    //? if that document does not exist then throw an error.
+    documentReference.update({
+      'full_name': 'Debnath  Partho',
+    }).catchError((val) {
+      log(val.toString());
+    });
+
+    //? delete that document if exists otherwise nothing to do.
+    // await documentReference.delete();
   }
 
   // Future<void> getValue() async {
